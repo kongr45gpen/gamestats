7ab1224b6fee93255a7dbcd5d274dd70252fd8f9
kongr45gpen <electrovesta@gmail.com>
2014-10-26 17:13:41 +0200

    Add league player teams

--- packs/bzstats/controllers/league.php
+++ packs/bzstats/controllers/league.php
@@ -14,17 +14,29 @@ class LeagueController extends \Qore\Controller {
 
     public function main_public(array $args) {
         $players = array(
-            'agron',
-            'brad',
-            'Chestal',
-            'Constitution',
-            'etigah',
-            'Mopar Madness',
-            'slime',
-            'Slyther366',
-            'quantum dot'
+            'agron' => array('team' => 'Sugar'),
+            'brad' => array('team' => 'Sugar'),
+            'Chestal' => array('team' => 'Oscar'),
+            'Constitution' => array('team' => 'Queen'),
+            'etigah' => array('team' => 'Oscar'),
+            'Mopar Madness' => array('team' => 'Lima'),
+            'osta' => array('team' => 'Roger'),
+            'slime' => array('team' => 'Queen'),
+            'Slyther366' => array('team' => 'Thomas'),
+            'quantum dot' => array('team' => 'Lima')
         );
 
+        // Sort by team name
+        uasort($players, function($val1, $val2) {
+            // Players with no team are last
+            if (!isset($val1['team'])) {
+                return 1;
+            } elseif (!isset($val2['team'])) {
+                return -1;
+            }
+
+            return -strnatcasecmp($val1['team'], $val2['team']);
+        });
 
         echo $this->twig->render('leagueOvso2014.html.twig', array(
             'players' => $players
