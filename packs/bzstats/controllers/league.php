95f191a6ddfb627c2fb63cc41230497ac6eef124
kongr45gpen <electrovesta@gmail.com>
2014-10-27 16:39:17 +0200

    Show whether league players are currently on a server

--- packs/bzstats/controllers/league.php
+++ packs/bzstats/controllers/league.php
@@ -6,6 +6,33 @@
  * @author kongr45gpen
  */
 class LeagueController extends \Qore\Controller {
+    /**
+     *
+     * @var \Packs\Bzstats\Models\Statsmodel
+     */
+    private $db;
+
+    /**
+     * our constructor - make sure we call the parent's constructor!
+     */
+    public function __construct() {
+        parent::__construct();
+
+        //build the time strings that we need for all the queries
+        //all SQL queries are passed GMT times
+        //since the dates are all GMT in the DB
+        //we pass $this->timezone setting to the DB, and the DB is responsible for timezone conversion
+        date_default_timezone_set('GMT');
+
+        $now = time();
+        $min5 = 60*5; //5 minutes in seconds
+
+        $this->curStart = date("Y-m-d H:i:s", $now-$min5);
+        $this->end = date("Y-m-d H:i:s", $now);
+
+        //grab a connection to our database
+        $this->db = \Qore\IoC::Get('bzstats.statsmodel');
+    }
 
     public function __pre() {
         parent::__pre();
@@ -25,6 +52,11 @@ class LeagueController extends \Qore\Controller {
             'Slyther366' => array('team' => 'Thomas'),
             'quantum dot' => array('team' => 'Lima')
         );
+
+        // Add player status
+        foreach ($players as $name => &$data) {
+            $data['servers'] = $this->db->getPlayerCurrentServers($name, $this->curStart, $this->end);
+        }
 
         // Sort by team name
         uasort($players, function($val1, $val2) {
