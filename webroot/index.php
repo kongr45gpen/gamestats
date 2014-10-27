62b26c666494c55e79dc91b448b3c19c58ad333a
kongr45gpen <electrovesta@gmail.com>
2014-10-27 16:30:59 +0200

    Fix URLs on sites where the app is installed in a subdirectory + minor formatting fixes

--- webroot/index.php
+++ webroot/index.php
@@ -2,17 +2,17 @@
 /**
  * Main application entry point for the Qore Framework
  * Copyright (C) 2012  Ian Farr
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -45,7 +45,7 @@ if (array_key_exists('HTTPS', $_SERVER)) {
 define('SITE_ROOT', HTTP_PROTO . $_SERVER['HTTP_HOST'] . SITE_SUBDIR);
 
 /**
- * bootstrap the system 
+ * bootstrap the system
  */
 require ROOT . DS . 'config' . DS . 'bootstrap.php';
 
